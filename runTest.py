c28c685767d719585a7c69af0e49fe70f8a465b9
Danny Hur <fourier412@gmail.com>
2020-01-13 23:37:47 -0800

    added korean.json; added upbeat.mp3

--- runTest.py
+++ runTest.py
@@ -11,43 +11,40 @@ class Test(object):
 
 	def __init__(self,testType):
 		self.testType = testType
+		testfile = f"{self.testType}.json"
 
-		file = f"{self.testType}.json"
-
-		with open(file,"r") as f:
+		with open(testfile,"r") as f:
 			self.test = json.load(f)
 
 		pygame.mixer.init()
 		self.burp = pygame.mixer.Sound(os.path.join('sound','burp.wav'))
 		self.yougotit = pygame.mixer.Sound(os.path.join('sound','you got it.wav'))
-		# music = pygame.mixer.music.load('music.mp3')
-		# pygame.mixer.music.play(-1)
+		backgroundmusic = pygame.mixer.music.load(os.path.join('sound','upbeat.mp3'))
+		pygame.mixer.music.play(-1)
 
 
 	def takeTest(self):
-		
-
 		score = 0
 		totalQuestion = 0
 
 		for k,v in self.test.items():
-
-			print(v['question'])
-			print(f"A. {v['a']}")
-			print(f"B. {v['b']}")
-			print(f"C. {v['c']}")
+			count = 1
+			print(f"\n\n{count}. {v['question']}")
+			print(f" a. {v['a']}")
+			print(f" b. {v['b']}")
+			print(f" c. {v['c']}")
 			ans = input("Answer: ").lower()
-
 			if ans == v['answer']:
 				self.yougotit.play()
 				print(f"\nCorrect! The answer is {ans}\n")
 				score +=1
 				totalQuestion +=1
+				count +=1
 			elif ans != v['answer']:
 				self.burp.play()
 				print("\nWrong!\n")
-
 				totalQuestion +=1
+				count +=1
 
 		if score == totalQuestion:
 			print("!!! PERFECT SCORE !!!")
@@ -58,13 +55,17 @@ class Test(object):
 def setArgParseAttributes():
     parser = argparse.ArgumentParser(
         description="operates tests")
-    parser.add_argument('-c','--civicTest',action='store_true',help="will execute Civic test")
+    parser.add_argument('-ci','--civicTest',action='store_true',help="will execute Civic test")
+    parser.add_argument('-ko','--koreanTest',action='store_true',help="will execute Korean test")
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
-    args = setArgParseAttributes()
-    if args.civicTest:
-    	x = Test(testType="civic")
-    	x.takeTest()
+	args = setArgParseAttributes()
+
+	if args.civicTest: testType="civic"
+	elif args.koreanTest: testType="korean"
+
+	x = Test(testType=testType)
+	x.takeTest()
